d18b7b12ddbbeea63d39edf544a61d204159c54c
Fab <nkurunziza2001gashati@gmail.com>
2022-12-21 09:38:10 +0000

    now we have added a private attribute size to our class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# an empty class Square that defines a square
+# for now we will add a private attribute 'size'
+"""
+    define class 'Square'
+"""
+
+
+class Square:
+    """
+	this class has private instance attribute 'size'
+    """
+    def __init__(self, size):
+	self.__size = size
