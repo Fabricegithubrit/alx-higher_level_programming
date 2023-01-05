f6d9d20dcc3a7173cd0d48b27d4e555f8e6cd8ad
fab <nkurunziza2001gashati@gmail.com>
2023-01-05 00:08:45 -0800

    an empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# an empty class Rectangle that defines a rectangle
+# You are not allowed to import any module
+"""
+    define class 'Rectangle'
+"""
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+in this project we will be dealing with classes and objects, it is object oriented programming
